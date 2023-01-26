78fbed9b64ad88e4bd46471a306a340c4bfd7a26
titou <titou.hellegouarch@gmail.com>
2023-01-26 20:07:57 +0100

    Formulaire v2

--- EL RANCHO/formulaire/formulaire.js
+++ EL RANCHO/formulaire/formulaire.js
@@ -1,12 +1,145 @@
-const container = document.getElementById('container');
-const loginButton = document.getElementById('login');
-const signUpButton = document.getElementById('signup');
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll(
+  'input[type="text"], input[type="password"]'
+);
+const progressBar = document.getElementById("progress-bar");
+let pseudo, email, password, confirmPass;
 
-signUpButton.addEventListener('click', () => {
-	console.log('click');
-	container.classList.add('panel-active');
-})
-loginButton.addEventListener('click', () => {
-	console.log('click-login');
-	container.classList.remove('panel-active');
-})
+/**
+ * 
+ * @param {prefixe class html} tag
+ * @param {diplay message d'erreur} message
+ * @param {boolean} valid
+ */
+const errorDisplay = (tag, message, valid) => {
+  const container = document.querySelector("." + tag + "-container");
+  const span = document.querySelector("." + tag + "-container > span");
+
+  if (!valid) {
+    // add class CSS error
+    container.classList.add("error");
+    // message error
+    span.textContent = message;
+  } else {
+    container.classList.remove("error");
+    span.textContent = message;
+  }
+};
+
+const pseudoChecker = (value) => {
+  // console(value)
+  if ((value.length < 3 || value.length > 20)) {
+    errorDisplay("pseudo", "Le pseudo doit faire entre 3 et 20 caractères");
+    pseudo = null;
+  } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
+    //  Regex accept : digit, case(UPPER, lower)
+    errorDisplay(
+      "pseudo",
+      "Le pseudo ne doit pas contenir de caractères spéciaux"
+    );
+    pseudo = null;
+  } else {
+    errorDisplay("pseudo", "", true);
+    // assigne string a value
+    pseudo = value;
+  }
+};
+
+const emailChecker = (value) => {
+  if (!value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
+    errorDisplay("email", "Le mail n'est pas valide");
+    email = null;
+  } else {
+    errorDisplay("email", "", true);
+    email = value;
+  }
+};
+
+const passwordChecker = (value) => {
+  progressBar.classList = "";
+
+  if (!value.match(
+      /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/
+    )) {
+    errorDisplay(
+      "password",
+      "Minimum de 8 caractères, une majuscule, un chiffre et un caractère spécial"
+    );
+    progressBar.classList.add("progressRed");
+    password = null;
+  } else if (value.length < 12) {
+    progressBar.classList.remove("progressRed");
+    progressBar.classList.add("progressBlue");
+    errorDisplay("password", "Mot de passe fort", true);
+    password = value;
+  } else {
+    progressBar.classList.add("progressGreen");
+    errorDisplay("password", "Mot de passe Blindé", true);
+    password = value;
+  }
+  if (confirmPass) confirmChecker(confirmPass);
+  // Rejoue le confim pass
+};
+
+const confirmChecker = (value) => {
+  if (value !== password) {
+    errorDisplay("confirm", "Les mots de passe ne correspondent pas");
+    confirmPass = false;
+  } else {
+    errorDisplay("confirm", "", true);
+    confirmPass = true;
+  }
+};
+/**
+ * Fonction main
+ * Appelle toutes les fonctions checkers en fonction de l'id du container.
+ * console.log(inputs)
+ */
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    // console.log(e.target.id);
+    // Le switch teste la valeur d'e.target.id
+    switch (e.target.id) {
+      case "pseudo":
+        pseudoChecker(e.target.value);
+        break;
+      case "email":
+        emailChecker(e.target.value);
+        break;
+      case "password":
+        passwordChecker(e.target.value);
+        break;
+      case "confirm":
+        confirmChecker(e.target.value);
+        break;
+      default:
+        nul;
+    }
+  });
+});
+
+form.addEventListener("submit", (e) => {
+  // empeche le comportement par défault
+  e.preventDefault();
+
+  if (pseudo && email && password && confirmPass) {
+    const data = {
+      pseudo,
+      email,
+      password,
+    };
+    console.log(data);
+
+    inputs.forEach((input) => (input.value = ""));
+    progressBar.classList = "";
+
+    // remettre les valeurs a null
+    pseudo = null;
+    email = null;
+    password = null;
+    confirmPass = null;
+    alert("Inscription validée !");
+  } else {
+    alert("veuillez remplir correctement les champs");
+  }
+});
